2d3914a12f1218cf22fbb27f5c0dddcd1bffe5a1
Rocky Racoon <mdoig5247@gmail.com>
2019-04-24 11:57:49 -0500

    define port in app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, redirect
-import hed_scrape
+import os, hed_scrape
 
 app = Flask(__name__)
 
@@ -9,5 +9,7 @@ def home():
 
     return render_template('index.html', links=links)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    # app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
